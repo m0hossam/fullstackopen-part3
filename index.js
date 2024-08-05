74bb650cdfa3423da807b16e60686d6f2609e0cf
m0hossam <mohossam1326@gmail.com>
2024-08-05 22:10:55 +0300

    Finish exercise 3.6

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+app.use(express.json()); // IMPORTANT: This uses middleware to parse JSON request payloads, otherwise 'req.body' is underfined
 const PORT = 3001;
 
-const persons = [
+let persons = [ // not const because we delete from it
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -25,9 +26,64 @@ const persons = [
     }
 ];
 
+app.get('/info', (req, res) => {
+    const date = new Date().toUTCString();
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`);
+});
+
 app.get('/api/persons', (req, res) => {
     res.json(persons);
-})
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    const person = persons.find(p => p.id === id);
+
+    if (person) {
+        res.json(person);
+    }
+    else {
+        res.status(404).end(); // Resource not found
+    }
+});
+
+const generateId = () => {
+    const minCeiled = Math.ceil(1);
+    const maxFloored = Math.floor(1000000);
+    let id = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+    while (persons.find(p => p.id === id)) { // generate random IDs until we get one that is not used
+        id = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+    }
+    return id;
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({ error: 'Name/Body is missing.' }); // Bad request
+    }
+
+    if (persons.find(p => p.name === body.name)) {
+        return res.status(400).json({ error: 'Name must be unique.' });
+    }
+
+    const person = {
+        id: generateId().toString(),
+        name: body.name,
+        number: body.number
+    };
+
+    persons = persons.concat(person);
+    res.json(person);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    persons = persons.filter(p => p.id !== id);
+    res.status(204).end(); // No content
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
